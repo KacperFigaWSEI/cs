082bb6db0b88fd0538ac8e6bf0307662d301773f
KacperFigaWSEI <kacper.figa1@microsoft.wsei.edu.pl>
2023-10-10 00:13:04 +0200

    PRIME_T

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -4,43 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        int _number;
-        int num_of_zeros;
 
-        do
+        int n = int.Parse(Console.ReadLine());
+
+        for(int i = 0; i < n; i++)
         {
-            Console.WriteLine("Podaj liczbę naturalną: ");
+            Console.WriteLine("Podaj liczbe: ");
+            int x = int.Parse(Console.ReadLine());
 
-            _number = int.Parse(Console.ReadLine());
-        } while ((_number <= 0));
+            if (x < 2 || !isPrime(x)) Console.WriteLine("NIE");
+            else Console.WriteLine("TAK");
+        }
+      
+    }
 
-        num_of_zeros = (_number.ToString().Length-1);
-
-        double start_scope = 1;
-        double end_scope = 10;
-
-        if(num_of_zeros > 0)
-        {
-            char lastChar = _number.ToString()[_number.ToString().Length - 1];
-            if (lastChar == '0')
-            {
-                start_scope = Math.Pow(10, num_of_zeros-1)+1;
-
-                if (start_scope == 2)
-                {
-                    start_scope = 1;
-                }
-
-                end_scope = Math.Pow(10, num_of_zeros);
+    static bool isPrime(int n) {
+        for (int i = 2; i <= Math.Sqrt(n); i++){
+            if (n % i == 0){
+                return false;
             }
-            else {
-                start_scope = Math.Pow(10, num_of_zeros) + 1;
-                end_scope = (start_scope -1) * 10;
-            }
-
-        };
-
-        Console.WriteLine(start_scope + " - " + end_scope);
+        }
+        return true;
     }
 
 }
