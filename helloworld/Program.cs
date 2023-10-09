6c325e3ff02928b859b9ace55e7c813c26739ac1
KacperFigaWSEI <kacper.figa1@microsoft.wsei.edu.pl>
2023-10-09 22:26:51 +0200

    first commit

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -4,7 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int a, b, c;
+
+        do
+        {
+            Console.WriteLine("Wpisz liczbe niewieksza niz 100");
+            a = int.Parse(Console.ReadLine());
+        } while (a > 100);
+
+        do
+        {
+            Console.WriteLine("Wpisz liczbe niewieksza niz 100");
+            b = int.Parse(Console.ReadLine());
+        } while (b > 100);
+
+        do
+        {
+            Console.WriteLine("Wpisz liczbe niewieksza niz 100");
+            c = int.Parse(Console.ReadLine());
+        } while (c > 100);
+
+        Console.WriteLine(a + b + c);
     }
+
 }
 
